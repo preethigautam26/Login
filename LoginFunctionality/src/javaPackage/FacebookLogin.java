ea0cebf256a3b4d0c7581b908fe0cbcef3856143
Amith Gautam <Amith Gautam@LAPTOP-UC8VD6T3>
2024-09-14 14:13:00 +0530

    added sysyo

---
+++ LoginFunctionality/src/javaPackage/FacebookLogin.java
@@ -0,0 +1,22 @@
+package javaPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FacebookLogin {
+
+	
+	
+	@Test
+	
+	public void login() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("preethis9008@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("1234");
+		driver.close();
+	}
+}
